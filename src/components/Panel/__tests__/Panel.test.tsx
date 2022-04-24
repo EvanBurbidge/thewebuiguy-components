7e9690f79d988152a8cbc0583a3b3e71b860c55e
Evan.Burbidge <burbidge@tcd.ie>
2022-04-24 10:47:07 +0100

    update panel test

--- src/components/Panel/__tests__/Panel.test.tsx
+++ src/components/Panel/__tests__/Panel.test.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
-import { SidePanel, SidePanelProps } from '..';
-import { render, screen } from '@testing-library/react';
+import { Panel } from '..'
+import { ModalTitle, ModalBody, ModalFooter } from '../..';
+import { render, screen, fireEvent } from '@testing-library/react';
 
-const defaultProps: SidePanelProps = {
-  
+const Title = () => <ModalTitle title="Test title" />
+const Body = () => <ModalBody Content="Some test text for the modal" />
+
+const defaultProps: any = {
+  open: true,
+  onClose: () => { },
+  buttonType: "primary",
 };
 
-const setup = (props = defaultProps) => render(<SidePanel {...props} />);
+const fn = jest.fn();
+
+const setup = (props = defaultProps) => render(<Panel PanelFooter={<ModalFooter confirmAction={fn} cancelAction={fn} cancelButtonText="cancel" confirmButtonText='confirm' buttonType={props.buttonType} />} PanelBody={<Body />} PanelHeader={<Title />} open={props.open} onClose={props.onClose} />);
+
 
 describe('SidePanel', () => {
-  it('renders', () => {
-    setup({children: 'foo'});
-    expect(screen.getByText('foo'));
+  it('renders', async () => {
+    setup();
+    screen.getByText("Test title");
+    screen.getByText("Some test text for the modal");
+    screen.getByText("confirm");
+    screen.getByText("cancel");
+
+    await fireEvent.click(screen.getByText("confirm"));
+    expect(fn).toHaveBeenCalled();
+
+    await fireEvent.click(screen.getByText("cancel"));
+    expect(fn).toHaveBeenCalledTimes(2);
   });
 });
