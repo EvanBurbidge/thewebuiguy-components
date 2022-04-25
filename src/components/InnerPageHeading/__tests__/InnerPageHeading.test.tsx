9b8a0395f74f1ec2d63195de551874e9883a8a86
Evan.Burbidge <burbidge@tcd.ie>
2022-04-25 20:32:07 +0100

    update tests

--- src/components/InnerPageHeading/__tests__/InnerPageHeading.test.tsx
+++ src/components/InnerPageHeading/__tests__/InnerPageHeading.test.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 import { CalendarIcon, CurrencyEuroIcon } from '@heroicons/react/solid'
 import { InnerPageHeading, InnerPageHeadingProps } from '..';
 import { render, screen } from '@testing-library/react';
+import { Button, ButtonOutline } from '../..';
+
+const Actions = () => (
+  <div className="flex">
+    <ButtonOutline id="secondary" onClick={() => { }}>Secondary</ButtonOutline>
+    <Button id="secondary" onClick={() => { }} classNames="ml-3">Primary</Button>
+  </div>
+)
 
 const defaultProps: InnerPageHeadingProps = {
   breadcrumbs: [
@@ -25,7 +33,8 @@ const defaultProps: InnerPageHeadingProps = {
     { text: "20/12/2022", Icon: CalendarIcon },
     { text: "100 - 120k", Icon: CurrencyEuroIcon },
   ],
-  title: "Some page title"
+  title: "Some page title",
+  Actions: <Actions />
 };
 
 const setup = (props = defaultProps) => render(<InnerPageHeading {...props} />);
@@ -38,5 +47,7 @@ describe('HeadingMetaData', () => {
     screen.getByText('Third');
     screen.getByText('20/12/2022');
     screen.getByText('100 - 120k');
+    screen.getByText('Primary');
+    screen.getByText('Secondary');
   });
 });
