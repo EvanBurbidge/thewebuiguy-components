2e8f26d13aaec50b488e610de0ec7ad8918e5f7c
EvanBurbidge <burbidge@tcd.ie>
2022-04-29 11:10:27 +0100

    fix child issue

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -46,12 +46,14 @@ export const Modal: React.FC<ModalProps> = ({
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            {ModalHeader}
-            <div className="relative inline-block px-4 pt-5 pb-4 overflow-hidden text-left align-bottom transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
-              <div className="mt-3 sm:mt-5">
-                {ModalBody}
+            <div>
+              {ModalHeader}
+              <div className="relative inline-block px-4 pt-5 pb-4 overflow-hidden text-left align-bottom transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+                <div className="mt-3 sm:mt-5">
+                  {ModalBody}
+                </div>
+                {ModalFooter}
               </div>
-              {ModalFooter}
             </div>
           </Transition.Child>
         </div>
