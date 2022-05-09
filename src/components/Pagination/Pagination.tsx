fb0a742ac353963193b1b6988c3829fd007b2bf7
Evan.Burbidge <burbidge@tcd.ie>
2022-05-09 22:53:07 +0100

    update pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -12,17 +12,19 @@ export interface PaginationProps {
 
 
 const containerClass = "flex justify-center h-auto align-center";
-const activeClass = 'cursor-pointer border-highlight text-highlight border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium';
+const activeClass = 'cursor-pointer !border-highlight !text-highlight !border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium';
 const inActiveClass = 'mb-4 cursor-pointer border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium';
 
 const getPageNumbers = (totalItems: number, itemsPerPage: number): number => Math.ceil((totalItems / itemsPerPage)) >> 0;
 
 export const Pagination: React.FC<PaginationProps> = ({ totalItems, itemsPerPage, currentPage, onPageChange, disableInitialCallback = false, initialPage = 0 }: PaginationProps) => {
-
   const handleOnPageChange = useCallback(({ selected = 0 }) => onPageChange(selected), [onPageChange]);
   const pageCount = useMemo(() => getPageNumbers(totalItems, itemsPerPage), [totalItems, itemsPerPage]);
   return (
     <ReactPaginate
+      pageCount={pageCount}
+      pageRangeDisplayed={10}
+      marginPagesDisplayed={4}
       initialPage={initialPage}
       pageClassName={inActiveClass}
       activeClassName={activeClass}
@@ -31,10 +33,7 @@ export const Pagination: React.FC<PaginationProps> = ({ totalItems, itemsPerPage
       previousClassName={inActiveClass}
       onPageChange={handleOnPageChange}
       containerClassName={containerClass}
-      pageRangeDisplayed={10}
-      marginPagesDisplayed={4}
       disableInitialCallback={disableInitialCallback}
-      pageCount={pageCount}
       renderOnZeroPageCount={() => null}
     />
   );
