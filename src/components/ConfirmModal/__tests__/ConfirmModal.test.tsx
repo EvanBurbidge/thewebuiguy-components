069a06f212f40f320fcc7af1c68403472d76f307
Evan.Burbidge <burbidge@tcd.ie>
2022-05-17 21:11:00 +0100

    update tests

--- src/components/ConfirmModal/__tests__/ConfirmModal.test.tsx
+++ src/components/ConfirmModal/__tests__/ConfirmModal.test.tsx
@@ -3,14 +3,24 @@ import { ConfirmModal, ConfirmModalProps } from '..';
 import { render, screen } from '@testing-library/react';
 
 const defaultProps: ConfirmModalProps = {
-  
+  open: true,
+  cancelAction: () => { },
+  confirmAction: () => { },
+  cancelText: "No way",
+  confirmText: "Maybe",
+  Content: "Do you want to do the thing",
+  type: "primary",
+  title: "do you want the thing"
 };
 
 const setup = (props = defaultProps) => render(<ConfirmModal {...props} />);
 
 describe('ConfirmModal', () => {
   it('renders', () => {
-    setup({children: 'foo'});
-    expect(screen.getByText('foo'));
+    setup();
+    screen.getByText('No way');
+    screen.getByText('Maybe');
+    screen.getByText('Do you want to do the thing');
+    screen.getByText('do you want the thing');
   });
 });
